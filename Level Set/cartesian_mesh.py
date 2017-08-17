85f99c2554cd5f30a83af007d9d5a608d67b86ef
MathematicianVogt <rvogt2@mac544-sas3219.math.ncsu.edu>
2017-08-17 12:34:02 -0400

    1

--- Level Set/cartesian_mesh.py
+++ Level Set/cartesian_mesh.py
@@ -9,16 +9,16 @@ class cartesian_mesh:
 	#nx - number steps in x direction
 	#nx - number steps in y direction
 	def __init__(self,a,b,c,d,nx,ny):
-		x = np.linspace(a,b,nx)
-		y = np.linspace(c,d,ny)
-		self.xx,self.yy = np.meshgrid(x, y, indexing = 'ij')
+		self.x = np.linspace(a,b,nx)
+		self.y = np.linspace(c,d,ny)
+		self.xx,self.yy = np.meshgrid(self.x, self.y, indexing = 'ij')
 		self.mesh = (self.xx,self.yy)
 	
 	def x(self):
 		return self.xx
 	def y(self):
 		return self.yy
-		
+
 	def return_point_on_mesh(self,i,j):
 		
 		try:
@@ -29,7 +29,14 @@ class cartesian_mesh:
 
 	def size(self):
 		return np.shape(self.mesh)
-
+	def h(self):
+		dx = 2.0*(self.x[1]-self.x[0])
+		dy=2.0*(self.y[1]-self.y[0])
+		return (dx,dy)
+	def half_h(self):
+		dx = (self.x[1]-self.x[0])
+		dy=(self.y[1]-self.y[0])
+		return (dx,dy)
 
 x = cartesian_mesh(0.0,1.0,0.0,2.0,100,99)
 print x.return_point_on_mesh(0,98)
