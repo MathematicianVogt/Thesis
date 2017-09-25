b84abd9896fbec2f52f2b807453ff43c5ef5b0da
MathematicianVogt <mathbosss@gmail.com>
2017-09-25 15:55:02 -0400

    1
    # Conflicts:
    #	Level Set/ex_mesh.pyc
    #	Level Set/ey_mesh.pyc
    #	Level Set/ez_mesh.pyc
    #	Level Set/hx_mesh.pyc
    #	Level Set/hy_mesh.pyc
    #	Level Set/hz_mesh.pyc
    #	Level Set/maxwell_plot.pyc

---
+++ eliptic interface/elliptic.py
@@ -0,0 +1,265 @@
+import matplotlib.pyplot as plt
+import math
+from compare_exact_numerical import compare_exact_numerical_2D
+from time_mesh import time_mesh
+import numpy as np
+from pde import *
+
+
+class ex:
+		# a<=x<=b
+	# c<=y<=d
+	#dx - spatial step in x
+	#dy - spatial step in y
+	#nx - number steps in x direction
+	#nx - number steps in y direction
+	def __init__(self,a,b,c,d,nx,ny,Tmax,nt,BCs,IC,phi,epsilon,mu):
+		#stencil update i+.5,j+.5
+		#time n+.5
+
+		self.x = np.linspace(a,b,nx)
+		dxt= (self.x[1]-self.x[0])/2.0
+		for i in range(0,len(self.x)):
+			self.x[i]+=dxt
+		#print self.x
+		self.y = np.linspace(c,d,ny)
+
+		self.x_list=self.x
+		self.y_list=self.y
+		self.xx,self.yy = np.meshgrid(self.x[:-1], self.y, indexing = 'ij')
+		self.xsize = len(self.x)-1
+		self.ysize=len(self.y)
+		self.mesh = (self.xx,self.yy)
+		self.time_mesh = time_mesh(Tmax,nt)
+		self.dt = self.time_mesh.time_step()
+		self.ex_sol=[]
+		self.IC=IC
+		self.add_ic()
+		self.phi = phi_ex(phi,self)
+		self.interface_grid = self.phi.set_irregular_regular_points()
+		self.epsilon=epsilon
+		self.mu=mu
+		self.BC=BCs
+
+	def get_phi(self):
+		return self.phi
+	def get_sol(self):
+		return (self.xx,self.yy,self.ex_sol)
+
+	def get_interface(self):
+		return (self.xx,self.yy,self.interface_grid)
+	def plot_interface(self):
+		plt.pcolor(self.xx, self.yy, self.interface_grid,cmap='cool')
+		plt.show()
+
+
+	def add_ic(self):
+		x1=self.xsize
+		x2=self.ysize
+		IC_cond = np.zeros((x1,x2))
+		for i in range(0,self.xsize):
+			for j in range(0,self.ysize):	
+				IC_cond[i,j] = self.IC(self.x_list[i],self.y_list[j])
+		self.ex_sol.append(IC_cond)
+
+	def enforce_boundary_conditons(self, t):
+		#bc dictionary
+		bc=self.BC
+		top = bc["top"]
+		left = bc["left"]
+		right=bc["right"]
+		bottom=bc["bottom"]
+
+		new_sol_boundary_conditions_enforced=np.zeros((self.xsize,self.ysize))
+
+		for i in range(0,self.xsize):
+			for j in range(0,self.ysize):
+
+				# #left_bc
+				# if(i==0 and j>=0):
+				# 	new_sol_boundary_conditions_enforced[i,j] = left(self.y_list[j],t)
+					
+				#bottom BC
+				if(j==0 and i>=0):
+					new_sol_boundary_conditions_enforced[i,j] = bottom(self.x_list[i],t)
+
+				#top BC
+				if(j==self.ysize-1 and i>=0):
+					new_sol_boundary_conditions_enforced[i,j] = top(self.x_list[i],t)
+				# #right bc
+				# if(i==self.xsize-1 and j>=0):
+				# 	new_sol_boundary_conditions_enforced[i,j] = right(self.y_list[j],t)
+
+		return new_sol_boundary_conditions_enforced
+
+	def x_list(self):
+		return self.xx
+	def y_list(self):
+		return self.yy
+
+	def one_d_x(self):
+		return self.x
+	def one_d_y(self):
+		return self.y
+
+	def return_point_on_mesh(self,i,j):
+		
+		if(i<0 or j <0):
+			return (None,None)
+
+		try:
+			return (self.x[i],self.y[j])
+		except:
+			return (None,None)
+
+
+	def size(self):
+		return np.shape(self.mesh)
+	#spatial steps
+	def h(self):
+		dx = self.x[1]-self.x[0]
+		dy=(self.y[1]-self.y[0])
+		return (dx,dy)
+	# def half_h(self):
+	# 	dx = (self.x[1]-self.x[0])
+	# 	dy=(self.y[1]-self.y[0])
+	# 	return (dx,dy)
+	def build_sol_regular(self,t,hz):
+		(dx,dy) = self.h()
+		mu=self.mu
+		epsilon=self.epsilon
+		dt =self.dt
+		previous_ex = self.ex_sol[-1]
+		ex = self.enforce_boundary_conditons(t)
+		# print np.shape(ex)
+		# print self.xsize
+		# print self.ysize
+		for i in range(0,len(self.x_list)-1):
+			for j in range(1,len(self.y_list)-1):
+				ex[i,j] = previous_ex[i,j] + (dt/(epsilon(self.x_list[i],self.y_list[j])*dy))*(hz[i,j] - hz[i,j-1])
+		self.ex_sol.append(ex)
+
+	def previous_sol(self):
+		return self.ex_sol[-1]
+class phi_ex:
+	def __init__(self,phi,mesh):
+		self.phi=phi
+		self.mesh=mesh
+
+	def set_irregular_regular_points(self):
+		mesh=self.mesh
+		a = mesh.size()
+		#print a
+		xsize = a[1]
+		ysize=a[2]
+		mesh_numbering = np.zeros((xsize,ysize))
+
+		a=mesh.size()
+		xsize=a[1]
+		ysize=a[2]
+		for i in range(0,xsize):
+
+			for j in range(0,ysize):
+				mesh_numbering[i,j] = self.regular_or_irregular(i,j)
+				#print (i,j)
+
+
+
+		return mesh_numbering
+
+	def evaluate(self,xi,yi):
+		return self.phi(xi,yi)
+	def evaluate_min(self,xi,yi):
+		if(xi is None or yi is None ):
+			return float("inf")
+		else:
+			return self.phi(xi,yi)
+	def evaluate_max(self,xi,yi):
+		if(xi is None  or yi is None):
+			return -float("inf")
+		else:
+			return self.phi(xi,yi)
+
+	def regular_or_irregular(self,i,j):
+		phi_min = self.phi_min(i,j)
+		phi_max = self.phi_max(i,j)
+
+
+		# if j==0:
+		# 	print (phi_min,phi_max,i,j)
+		# 	print self.mesh.return_point_on_mesh(i,j)
+
+		return int(phi_min*phi_max<= 0)
+
+	def phi_min(self,i,j):
+		
+
+		p1=self.mesh.return_point_on_mesh(i,j)
+		p2=self.mesh.return_point_on_mesh(i,j+1)
+		p3=self.mesh.return_point_on_mesh(i,j-1)
+
+
+
+		p1=self.evaluate_min(p1[0],p1[1])
+		p2 = self.evaluate_min(p2[0],p2[1])
+		p3=self.evaluate_min(p3[0],p3[1])
+		return min(p1,p2,p3)
+
+	def phi_max(self,i,j):
+		p1=self.mesh.return_point_on_mesh(i,j)
+		p2=self.mesh.return_point_on_mesh(i,j+1)
+		p3=self.mesh.return_point_on_mesh(i,j-1)
+
+
+
+		p1=self.evaluate_max(p1[0],p1[1])
+		p2 = self.evaluate_max(p2[0],p2[1])
+		p3=self.evaluate_max(p3[0],p3[1])
+		return max(p1,p2,p3)
+
+	def gradient(self,i,j):
+		x=self.x_list[i]
+		y=self.y_list[j]
+		(dx,dy) = self.mesh.h()
+		phi_x = (self.phi(x+dx,y) + self.phi(x,y))/(2.0*dx)
+		phi_y = (self.phi(x,y+dy) + self.phi(x,y))/(2.0*dy)
+
+		return (phi_x,phi_y)
+	def projection_and_get_angle(self,x1,y1,alpha):
+		grad=self.gradient(x1,y1)
+		x_star = (x1 + alpha*grad[0], y1 + alpha*grad[1])
+		normal_at_interface = self.gradient(x_star[0],x_star[1])
+		theta = math.acos((normal_at_interface[0])/math.sqrt(normal_at_interface[0]**2 + normal_at_interface[0]**2))
+		return (theta,x_star)
+
+	def theta_matrix(self,irregular_grid_point_matrix):
+
+		theta_matrix = np.zeros(self.xsize,self.ysize)
+		for i in range(0,self.xsize):
+			for j in range(0,self.ysize):
+				if (irregular_grid_point_matrix[i,j]==1):
+					(theta,x_star) = self.projection_and_get_angle(self.x_list[i],self.y_list[j])
+					theta_matrix[i,j] = theta
+				else:
+					pass
+		return theta_matrix
+	def curv(self,i,j):
+		grad = self.gradient(i,j)
+		(dx,dy) = self.mesh.h()
+		phi_x=grad[0]
+		phi_y=grad[1]
+		phi_xx = (self.phi(self.x_list[i] - dx,self.y_list[j]) - 2.0*self.phi(self.x_list[i],self.y_list[j]) + self.phi(self.x_list[i] + dx,self.y_list[j]))/dx**2
+		phi_yy = (self.phi(self.x_list[i],self.y_list[j]-dy) - 2.0*self.phi(self.x_list[i],self.y_list[j]) + self.phi(self.x_list[i] ,self.y_list[j] + dy))/dy**2
+		phi_xy = (self.phi(self.x_list[i] + dx,self.y_list[j] +dy) -self.phi(self.x_list[i] + dx,self.y_list[j] -dy) -self.phi(self.x_list[i] - dx,self.y_list[j] +dy)  + self.phi(self.x_list[i] - dx,self.y_list[j] -dy)  )/(4.0*dx*dy)
+
+		k = (phi_xx*phi_y**2 -2.0*phi_x*phi_y*phi_xy + phi_yy*phi_x**2)/(1e-15 +math.pow(phi_x**2 + phi_y**2),1.5)
+		return k
+	def normal(self,i,j):
+		grad = self.gradient(i,j)
+		phi_x = grad[0]
+		phi_y=grad[1]
+		norm = math.sqrt(1e-15 + phi_x**2 + phi_y**2)
+		return (phi_x/norm,phi_y/norm)
+
+
+
---
+++ eliptic interface/test.py
@@ -0,0 +1,105 @@
+from ex_mesh import *
+from ey_mesh import *
+from ez_mesh import *
+from hx_mesh import *
+from hy_mesh import *
+from hz_mesh import *
+import pylab as plt
+import numpy as np
+from solution_normal import *
+from time_mesh import *
+from maxwell_plot import *
+
+a=0
+b=1
+c=0
+d=1
+nx=100
+ny=100
+Tmax=1
+nt=10000
+BC = {}
+basic_bc = lambda x,y:1.0
+BC["top"] = basic_bc
+BC["bottom"] = basic_bc 
+BC["left"] = basic_bc
+BC["right"] = basic_bc
+IC = lambda x,y: x**2 + y**2
+xo = .5
+yo=.5
+r=.25
+epsilon = lambda x,y : 1.0
+mu = lambda x,y : 1.0
+#phi = lambda x,y : (x-xo)**2 + (y-yo)**2. - r**2
+
+
+phi = lambda x,y : x-.5
+#phi = lambda x,y : y-.5
+
+
+p1 = ex(a,b,c,d,nx,ny,Tmax,nt,BC,IC,phi,epsilon,mu)
+p2 = ey(a,b,c,d,nx,ny,Tmax,nt,BC,IC,phi,epsilon,mu)
+p3 = ez(a,b,c,d,nx,ny,Tmax,nt,BC,IC,phi,epsilon,mu)
+p4 = hx(a,b,c,d,nx,ny,Tmax,nt,BC,IC,phi,epsilon,mu)
+p5 = hy(a,b,c,d,nx,ny,Tmax,nt,BC,IC,phi,epsilon,mu)
+p6 = hz(a,b,c,d,nx,ny,Tmax,nt,BC,IC,phi,epsilon,mu)
+
+t1 = time_mesh(Tmax,nt)
+t2 =time_mesh(Tmax,2*nt)
+
+a =solution(p1,p2,p3,p4,p5,p6,t1,t2)
+a.solve_TE()
+a.solve_TM()
+maxwellplot=maxwell_plot(p1,p2,p3,p4,p5,p6,Tmax,nt)
+maxwellplot.plot_contour("movielol")
+
+# p1.plot_interface()
+# p2.plot_interface()
+# c2=p2.get_interface()
+# print c2[2]
+# p3.plot_interface()
+# p4.plot_interface()
+# p5.plot_interface()
+# p6.plot_interface()
+
+# c1=p1.get_interface();
+# c2=p2.get_interface();
+# c3=p3.get_interface();
+# c4=p4.get_interface();
+# c5=p5.get_interface();
+# c6=p6.get_interface();
+
+
+# # print np.count_nonzero(c1[2])
+# # print np.count_nonzero(c2[2])
+# # print np.count_nonzero(c3[2])
+# # print np.count_nonzero(c4[2])
+# # print np.count_nonzero(c5[2])
+# # print np.count_nonzero(c6[2])
+
+
+
+# # # plt.pcolor(c1[0],c1[1],c1[2],cmap='cool')
+# # # plt.show()
+
+#plot interfaces
+# plt.figure(1)
+# plt.subplot(231)
+# plt.pcolor(c1[0], c1[1], c1[2],cmap='cool')
+# plt.title("E1")
+# plt.subplot(232)
+# plt.pcolor(c2[0], c2[1], c2[2],cmap='cool')
+# plt.title("E2")
+# plt.subplot(233)
+# plt.pcolor(c3[0], c3[1], c3[2],cmap='cool')
+# plt.title("E3")
+# plt.subplot(234)
+# plt.pcolor(c4[0], c4[1], c4[2],cmap='cool')
+# plt.title("H1")
+# plt.subplot(235)
+# plt.pcolor(c5[0], c5[1], c5[2],cmap='cool')
+# plt.title("H2")
+# plt.subplot(236)
+# plt.pcolor(c6[0], c6[1], c6[2],cmap='cool')
+# plt.title("H3")
+# plt.show()
